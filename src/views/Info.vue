f2bb0ca0b4d719081149a8ced8da8a205fbb521f
fta <1018598742@qq.com>
2020-10-14 18:48:42 +0800

    add:first hello-ui router-vuex

---
+++ src/views/Info.vue
@@ -0,0 +1,18 @@
+<template>
+<!--  <div>-->
+<!--    hello info component-->
+<!--  </div>-->
+  <div class="info">
+    <h1>hello info component</h1>
+  </div>
+</template>
+
+<!--<script>-->
+<!--  export default {-->
+<!--    name: 'Info'-->
+<!--  }-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
